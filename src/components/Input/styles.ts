fdf2cf1820149d2304fd17b1872dd313f8b9f801
Diego Souza <diegosouza@tradecom.com.br>
2022-06-18 20:50:22 -0300

    Refatorando input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,12 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: any;
+  isFilled: any;
+
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
